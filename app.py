901a1c38cea0056e6abe6bcd9c3feff7bb41729f
Mahmoudakram <elsha017@umn.edu>
2018-03-20 22:32:35 -0500

    d

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 from flask_restful import Resource, Api, abort
 from string import Template
-from flask import requests 
+import requests 
 from flask import request
 app = Flask(__name__)
 
